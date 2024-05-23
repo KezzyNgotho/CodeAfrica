abd803e1619cb7036e56fe62612689f15143d885
KezzyNgotho <keziengotho18@gmail.com>
2024-05-23 18:03:01 +0300

    const

--- Health/helphy/vite.config.ts
+++ Health/helphy/vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
+import path from 'path';
 
-// https://vitejs.dev/config/
+dotenv.config({ path: '../../.env' });
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Skip certain warnings
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
+        // Use default for everything else
+        warn(warning);
+      },
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:4943",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
+  ],
+  resolve: {
+    alias: {
+      'declarations': fileURLToPath(new URL('../declarations', import.meta.url)),
+      '@': path.resolve(__dirname, './src'), // Alias for src directory
+    },
+  },
+});
