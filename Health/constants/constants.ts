3b95b65f7af7ead4d8babfc779dc2ec0ca95d872
KezzyNgotho <keziengotho18@gmail.com>
2024-05-24 16:23:38 +0300

    path

--- Health/constants/constants.ts
+++ Health/constants/constants.ts
@@ -1,5 +1,5 @@
 /* import userRegistration from '/home/keziah/CodeAfrica/Health/helphy/src/abi/UserRegistration.json'; */
- import userRegistration from '/home/keziah/CodeAfrica/Health/abi/UserRegistration.json';;
+ import userRegistration from '../abi/UserRegistration.json';
 
 const userRegistrationABI = userRegistration.abi;
 const userRegistrationAddress = '0x00E2cc9a80B93DA6Ab31DB4f9FEBa29F9330C6b0';
